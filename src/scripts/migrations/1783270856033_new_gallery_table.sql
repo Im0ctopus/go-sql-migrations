405ab830370a5b3eea7993b472f75730658b04d4
Im0ctopus <leoferreira1712@gmail.com>
2026-07-05 22:17:38 +0100

    add basic migration script with examples

---
+++ src/scripts/migrations/1783270856033_new_gallery_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE gallery (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    "key" TEXT,
+    url TEXT
+);
---
+++ src/scripts/migrationsRunner/migrationsRunner.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+var migrationTableQuery = `
+CREATE TABLE IF NOT EXISTS migrations (
+id SERIAL PRIMARY KEY,
+"scriptName" TEXT NOT NULL,
+timestamp BIGINT NOT NULL
+);
+`
+
+func main() {
+	ctx := context.Background()
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	migrations, err := os.ReadDir("../migrations/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := pgx.Connect(ctx, os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close(ctx)
+
+	_, err = db.Exec(ctx, migrationTableQuery)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	query := `SELECT "scriptName" from migrations`
+	rows, err := db.Query(ctx, query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var appliedMigrations []string
+	for rows.Next() {
+		var scriptName string
+		err := rows.Scan(&scriptName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		appliedMigrations = append(appliedMigrations, scriptName)
+	}
+
+	query = `INSERT INTO migrations ("timestamp", "scriptName")
+	VALUES ($1, $2)
+	`
+	for _, migration := range migrations {
+		if slices.ContainsFunc(appliedMigrations, func(appliendMigration string) bool { return appliendMigration == migration.Name() }) {
+			continue
+		}
+
+		migrationScript, err := os.ReadFile("../migrations/" + migration.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = db.Exec(ctx, string(migrationScript))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = db.Exec(ctx, query, time.Now().Format("200601021504"), migration.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Applied script " + migration.Name())
+	}
+
+}
